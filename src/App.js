5cc1d826e3ae9fc556278c47f26d1fc33f79549e
Walter Lee <walter.lee9@gmail.com>
2019-06-04 11:23:04 -0500

    first layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
+import Concepts from './components/concepts';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Reacting to Concepts</h1>
+      </header>
 
+      <Concepts />
     </div>
   );
 }
---
+++ src/components/concepts.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Concepts extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {
+
+    //     };
+    // }
+
+
+
+    render() {
+        return (
+            <div className="concept-container">
+                <div>React is...</div>
+            </div>
+        );
+    }
+}
+
+export default Concepts;
