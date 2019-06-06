f0fd9fe13900df4a883994e24054e02433f5a934
walterlee <walter.lee9@gmail.com>
2019-06-06 14:48:53 -0500

    prep for markup display

--- src/components/concepts.js
+++ src/components/concepts.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import test from './markup/test.md'
+
 
 class Concepts extends Component {
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
 
-    //     this.state = {
+        this.state = {
+            markup: []
+        };
+    }
 
-    //     };
-    // }
+    componentDidMount() {
+        let concept = { test };
 
-
+        for (let i = 0; concept.length < i; i++) {
+            fetch(concept)
+        }
+    }
 
     render() {
         return (
             <div className="concept-container">
                 <div>React is...</div>
+                <div className="concept-container">
+                    <div>{test}</div>
+                </div>
             </div>
+
         );
     }
 }
