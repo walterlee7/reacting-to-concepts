5f8b0a035241ab3b3ac003e5272b7b79948e361d
walterlee <walter.lee9@gmail.com>
2019-06-07 12:09:42 -0500

    gh pages prep

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1 className="App-title">Reacting to Concepts</h1>
             </header>
-
             <Concepts />
         </div>
     );
